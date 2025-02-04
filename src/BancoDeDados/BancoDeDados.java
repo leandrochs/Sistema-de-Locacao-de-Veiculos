b988fc4b78857261d8bd9f12097456b3234af063
leandrochs <leandrochagask@gmail.com>
2025-02-04 19:22:55 -0300

    adiciona classe que simula banco de dados

---
+++ src/BancoDeDados/BancoDeDados.java
@@ -0,0 +1,27 @@
+package BancoDeDados;
+
+import Clientes.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BancoDeDados {
+    public static List<Cliente> listaClientes = new ArrayList<>();
+
+    public static void adicionaCliente(Cliente cliente) {
+        listaClientes.add(cliente);
+    }
+
+    public static void imprimirClientes() {
+        for (Cliente cliente : listaClientes) {
+            System.out.println(cliente);
+        }
+    }
+
+    public static void getUltimoCliente() {
+        if (listaClientes.isEmpty()) {
+            System.out.println("Sem clientes cadastrados!");;
+        }
+        System.out.println(listaClientes.getLast());
+    }
+}
