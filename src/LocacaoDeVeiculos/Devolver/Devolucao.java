36d6dc8be49c07762d6b03a6b0fdc900e55abecd
leandrochs <leandrochagask@gmail.com>
2025-02-07 15:38:52 -0300

    logica para devolucao do veiculo

---
+++ src/LocacaoDeVeiculos/Devolver/Devolucao.java
@@ -0,0 +1,30 @@
+//package LocacaoDeVeiculos.Devolver;
+//
+//import java.util.Scanner;
+//
+//public class Devolucao {
+//    Scanner scanner = new Scanner(System.in);
+//
+//
+//    ALUGAR::
+//    apresentarVeiculosDisponiveis;
+//    escolherVeiculo;
+//    apresentarClientes;
+//    escolherCliente;
+//    escolherDataDevolucao;
+//    confirmarLocacao;
+//    criarLocacao;
+//        atualizarDisponibilidadeVeiculo;
+//        armazenarLocacao;
+//        Se ERRO: reverterDisponibilidadeVeiculo;
+//
+//    DEVOLUÇÃO::
+//    listarVeiculosAlugados;
+//    escolherDevolução;
+//    confirmarDevolucao;
+//    finalizarLocacao;
+//        atualizarDisponibilidadeVeiculo;
+//        removerLocacao;
+//        Se ERRO: reverterDisponibilidadeVeiculo;
+//
+//}
