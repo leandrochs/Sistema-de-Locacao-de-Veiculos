e91a29718ef0c774fec0d1d4bf2639183bbbeb57
Stefanie-Grou <stefanielima@ymail.com>
2025-02-07 21:08:59 -0300

    Adicionando validação de usuário PJ e PF

--- src/Clientes/Cliente.java
+++ src/Clientes/Cliente.java
@@ -1,5 +1,7 @@
 package Clientes;
 
+import java.util.Scanner;
+
 public class Cliente {
     private final int CPF_SIZE = 11;
     private final int  CNPJ_SIZE = 14;
@@ -8,12 +10,14 @@ public class Cliente {
     private String idNumber;
     private String adress;
     private String telephoneNumber;
+    private String tipoDeCliente;
 
     public Cliente(String name, String idNumber, String adress, String telephoneNumber) {
         this.name = name;
         this.idNumber = idNumber;
         this.adress = adress;
         this.telephoneNumber = telephoneNumber;
+        setIdNumber(idNumber);
     }
 
     public String getName() {
@@ -28,43 +32,51 @@ public class Cliente {
         return idNumber;
     }
 
-    public void setIdNumber(String idNumber) {
+
+public void setIdNumber(String idNumber) {
+    boolean valid;
+    Scanner scanner = new Scanner(System.in);
+    do {
         int intLength = idNumber.length();
-        //CPF, CNPJ ou erro
-//        if (intLength == CPF_SIZE) {
-//
-//        } else if (intLength == CNPJ_SIZE) {
-//
-//        }
-//        else {
-//            //exception
-//        }
-        this.idNumber = idNumber;
-    }
+        if (intLength == CNPJ_SIZE) {
+            this.tipoDeCliente = "PJ";
+            this.idNumber = idNumber;
+            valid = true;
+        } else if (intLength == CPF_SIZE) {
+            this.tipoDeCliente = "PF";
+            this.idNumber = idNumber;
+            valid = true;
+        } else {
+            System.out.println("IdNumber invalido. Por favor, insira novamente:");
+            idNumber = scanner.nextLine();
+            valid = false;
+        }
+    } while (!valid);
+}
 
     public String getAdress() {
-        return adress;
-    }
+    return adress;
+}
 
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
+public void setAdress(String adress) {
+    this.adress = adress;
+}
 
-    public String getTelephoneNumber() {
-        return telephoneNumber;
-    }
+public String getTelephoneNumber() {
+    return telephoneNumber;
+}
 
-    public void setTelephoneNumber(String telephoneNumber) {
-        this.telephoneNumber = telephoneNumber;
-    }
+public void setTelephoneNumber(String telephoneNumber) {
+    this.telephoneNumber = telephoneNumber;
+}
 
-    @Override
-    public String toString() {
-        return "Cliente{" +
-                "name='" + name + '\'' +
-                ", idNumber='" + idNumber + '\'' +
-                ", adress='" + adress + '\'' +
-                ", telephoneNumber='" + telephoneNumber + '\'' +
-                '}';
-    }
+@Override
+public String toString() {
+    return "Cliente{" +
+            "name='" + name + '\'' +
+            ", idNumber='" + idNumber + '\'' +
+            ", adress='" + adress + '\'' +
+            ", telephoneNumber='" + telephoneNumber + '\'' +
+            '}';
+}
 }
